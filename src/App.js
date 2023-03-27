a78bff345b806be06b313d65790b5613ee15000d
Marek Juričko <marek.juricko@azet.sk>
2023-03-27 22:51:53 +0200

    App.js - added routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import SharedLayout from "./pages/SharedLayout"
+import Home from "./pages/Home/Home"
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+        <Routes>
+
+            <Route path="/" element={ <SharedLayout/> }>
+              <Route index element={ <Home/> }/>
+            </Route>
+
+        </Routes>
+    </BrowserRouter>
   )
 }
 
